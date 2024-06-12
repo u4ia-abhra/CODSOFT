03ee26849792fa64397c770cf60a8a4a9cdc5b35
u4ia-abhra <157137147+u4ia-abhra@users.noreply.github.com>
2024-06-12 15:05:54 +0530

    Update simple calculator.py

--- simple calculator.py
+++ simple calculator.py
@@ -1,19 +1,21 @@
-a=int(input("Enter first operand: "))
-b=int(input("Enter second operand: "))
-c=input('''
-Enter operator
-+ for addition
-- for subtraction
-* for multiplication
-/ for division
+# Design a simple calculator with basic arithmetic operations.
+# Prompt the user to input two numbers and an operation choice.
+# Perform the calculation and display the result.
+a = eval(input("Enter 1st operand: "))
+b = eval(input("Enter 2nd operand: "))
+ch = input('''Enter 
++ for addition,
+- for subtraction, 
+* for multiplication,
+/ for division:
 ''')
-if(c=='+'):
+if (ch=="+"):
     print(a, "+", b, "=", a + b)
-elif(c=='-'):
+elif (ch=="-"):
     print(a, "-", b, "=", a - b)
-elif(c=='*'):
+elif (ch=="*"):
     print(a, "*", b, "=", a * b)
-elif(c=='/'):
+elif (ch=="/"):
     print(a, "/", b, "=", a / b)
 else:
-    print("Invalid operator")
+    print("Invalid Input")
