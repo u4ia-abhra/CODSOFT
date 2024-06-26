d7e37b1631eabd6e4271ba97210674a1b020f3f6
u4ia-abhra <abhrajitgh@gmail.com>
2024-06-26 13:38:32 +0530

    Update Rock paper Scissor game.py

--- Rock-Paper-Scissors Game/Rock paper Scissor game.py
+++ Rock-Paper-Scissors Game/Rock paper Scissor game.py
@@ -1,62 +1,57 @@
-#Rock Paper Scissor game
 import random
-l=["Rock","Paper","Scissor"]
-n=int(input("Enter number of rounds : "))
-x=0
-y=0
-for i in range(n):
-    Cc=random.choice(l)
-    ui=int(input('''Enter 
-    0 for Rock
-    1 for Paper
-    2 for Scissor
-    '''))
-    if(ui==0 or ui==1 or ui==2):
-        Uc = l[ui]
-    else:
-        print("Invalid input")
-        break;
-    if(Uc==Cc):
-        print("User's choice : ", Uc)
-        print("Computer's choice : ", Cc)
+user_score = 0
+computer_score = 0
+while True:
+    options = ["Rock", "Paper", "Scissor"]
+    while True:
+        user_input = int(input("Enter 1 for Rock,2 for Paper,3 for Scissor: "))
+        if user_input == 1 or user_input == 2 or user_input == 3:
+            break
+        else:
+            print("Invalid Input")
+    user_choice = options[user_input - 1]
+    print("User's choice: ", user_choice)
+    computer_choice = random.choice(options)
+    print("Computer's choice: ", computer_choice)
+    if user_choice == computer_choice:
         print("Round tied")
-        x=x+1
-        y=y+1
-    elif(Uc=="Rock"):
-        print("User's choice : ", Uc)
-        print("Computer's choice : ", Cc)
-        if(Cc=="Scissor"):
+    elif user_choice == "Rock":
+        if computer_choice == "Scissor":
             print("User wins round")
-            x=x+1
+            user_score += 1
         else:
             print("Computer wins round")
-            y=y+1
-    elif (Uc == "Paper"):
-        print("User's choice : ", Uc)
-        print("Computer's choice : ", Cc)
-        if (Cc == "Rock"):
+            computer_score += 1
+    elif user_choice == "Paper":
+        if computer_choice == "Rock":
             print("User wins round")
-            x = x + 1
+            user_score += 1
         else:
             print("Computer wins round")
-            y = y + 1
-    elif (Uc == "Scissor"):
-        print("User's choice : ", Uc)
-        print("Computer's choice : ", Cc)
-        if (Cc == "Paper"):
-            print("User wins round")
-            x = x + 1
-        else:
-            print("Computer wins round")
-            y = y + 1
+            computer_score += 1
     else:
-        print("Logical error")
-        break;
-print("User score: ",x)
-print("Computer score: ",y)
-if(x>y):
-    print("User wins game")
-elif(x<y):
-    print("User loses game")
-else:
-    print("Game tied")
+        if computer_choice == "Paper":
+            print("User wins round")
+            user_score += 1
+        else:
+            print("Computer wins round")
+            computer_score += 1
+    print("User score: ", user_score)
+    print("Computer score", computer_score)
+    while True:
+        repeat = int(input("Enter 0 to end game, 1 to play again: "))
+        if repeat == 0 or repeat == 1:
+            break
+        else:
+            print("Invalid Input")
+    if repeat == 0:
+        print("Game ended")
+        print("User score: ", user_score)
+        print("Computer score", computer_score)
+        if user_score == computer_score:
+            print("Game tied")
+        elif user_score > computer_score:
+            print("User wins game")
+        else:
+            print("Computer wins game")
+        break
